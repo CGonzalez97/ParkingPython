94dc35238699029f6fe0550c441dfe8c54e01205
Carlos <cgl76490@gmail.com>
2020-12-14 19:45:32 +0100

    Textos de vistas anyadidos

---
+++ vistas/vista_general.py
@@ -0,0 +1,9 @@
+class Vista_General():
+    def __init__(self):
+        pass
+
+    def preguntar_tipo_usuario(self):
+        return 'Seleccione tipo de usuario.' \
+               '\n1.Gestor.' \
+               '\n2.Cliente.' \
+               '\n0.Salir.'

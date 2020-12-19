6bece3e4721e45003d7aa43daae76fab235dc5bb
Carlos <cgl76490@gmail.com>
2020-12-19 20:56:19 +0100

    Pickle anyadido, sin testear

--- repositorios/cobro_repositorio.py
+++ repositorios/cobro_repositorio.py
@@ -1,6 +1,9 @@
+import pickle
+
 class Cobro_Repositorio:
     def __init__(self):
         self.__lista_cobros = []
+        self.__directorio = '../persistencia/cobros.pckl'
 
     @property
     def lista_cobros(self):
@@ -8,3 +11,15 @@ class Cobro_Repositorio:
     @lista_cobros.setter
     def lista_cobros(self, lista):
         self.__lista_cobros = lista
+    @property
+    def directorio(self):
+        return self.__directorio
+    @directorio.setter
+    def directorio(self, directorio):
+        self.__directorio = directorio
+
+    def guardar(self):
+        fichero = open(self.directorio,'wb')
+        pickle.dump(self.lista_cobros,fichero)
+        fichero.close()
+
